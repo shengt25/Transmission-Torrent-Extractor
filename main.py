4e7168410452cc2e10b482d305d770f024c6173a
Sheng Tai <alxt9@outlook.com>
2024-01-24 18:07:23 +0200

    enhancement: ask input in runtime rather than modify main.py file. close#1

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 import os.path
 from pathlib import Path
-import readline  # Don't delete this, so you can use arrow key when input string
+import readline  # Don't delete this, otherwise arrow key cannot be used when inputting
 
 from execute import execute
 
 # ------------------------SETTINGS-------------------------------
-manually = False
-resume_file_path = "/path/to/resume"
-torrent_file_path = "/path/to/torrents"
-debug = False  # Just output the command to be executed, nothing will happen to disk
+test_run = False  # Just output the command to be executed, nothing will happen to disk
 # ---------------------------------------------------------------
 
+tr_config_path = os.path.join(str(Path.home()), ".config/transmission")
+resume_file_path = os.path.join(tr_config_path, "resume")
+torrent_file_path = os.path.join(tr_config_path, "torrents")
 
-if manually is False:
-    tr_config_path = os.path.join(str(Path.home()), ".config/transmission")
-    if not os.path.exists(tr_config_path):
-        print("Transmission config path not found, please edit main.py to manually set the path")
-        exit(1)
-    else:
-        print(f"Transmission config path found: {tr_config_path}")
-        resume_file_path = os.path.join(tr_config_path, "resume")
-        torrent_file_path = os.path.join(tr_config_path, "torrents")
-        target_base = input("Press ENTER to extract to current folder, or input a new one: ")
-        if target_base == "":
-            target_base = os.path.join(os.path.dirname(os.path.realpath(__file__)), "extract")
+while not os.path.exists(resume_file_path) or not os.path.exists(torrent_file_path):
+    tr_config_path = input("Data not found, please specify transmission's config path:")
+    resume_file_path = os.path.join(tr_config_path, "resume")
+    torrent_file_path = os.path.join(tr_config_path, "torrents")
 
-        execute(resume_file_path, torrent_file_path, target_base, debug)
+print(f"Using config path: {tr_config_path}")
+target_base = input("Input path to save torrents, or press Enter to use current location:")
+if target_base == "":
+    target_base = os.path.join(os.path.dirname(os.path.realpath(__file__)), "torrent_extract")
+
+execute(resume_file_path, torrent_file_path, target_base, test_run)
